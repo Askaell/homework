4e3cfcdd253b3f5e119d0ef36d53b4eae7cfdbb6
Askaell <andrey.b176@gmail.com>
2021-04-25 16:17:40 +0300

    fix db fields of item model

--- pkg/models/item.go
+++ pkg/models/item.go
@@ -5,9 +5,9 @@ type Item struct {
 	Name          string  `json:"name" db:"name"`
 	Description   string  `json:"description" db:"description"`
 	Price         float32 `json:"price" db:"price"`
-	DiscountPrice float32 `json:"discountPrice" db:"discountPrice"`
+	DiscountPrice float32 `json:"discountPrice" db:"discountprice"`
 	Discount      float32 `json:"discount" db:"discount"`
-	DayItem       bool    `json:"dayItem" db:"dayItem"`
-	VendorCode    string  `json:"vendorCode" db:"vendorCode"`
+	DayItem       bool    `json:"dayItem" db:"dayitem"`
+	VendorCode    string  `json:"vendorCode" db:"vendorcode"`
 	Category      string  `json:"category" db:"category"`
 }
