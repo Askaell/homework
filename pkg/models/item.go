97e817d49034d7a82de8b455816bbd5515e257e6
Askaell <andrey.b176@gmail.com>
2021-04-25 14:53:38 +0300

    change item model

--- pkg/models/item.go
+++ pkg/models/item.go
@@ -1,8 +1,13 @@
 package models
 
 type Item struct {
-	Id          int     `json:"id" db:"id"`
-	Name        string  `json:"name" db:"name"`
-	Description string  `json:"description" db:"description"`
-	Price       float32 `json:"price" db:"price"`
+	Id            int     `json:"id" db:"id"`
+	Name          string  `json:"name" db:"name"`
+	Description   string  `json:"description" db:"description"`
+	Price         float32 `json:"price" db:"price"`
+	DiscountPrice float32 `json:"discountPrice" db:"discountPrice"`
+	Discount      float32 `json:"discount" db:"discount"`
+	DayItem       bool    `json:"dayItem" db:"dayItem"`
+	VendorCode    string  `json:"vendorCode" db:"vendorCode"`
+	Category      string  `json:"category" db:"category"`
 }
