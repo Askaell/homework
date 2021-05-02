cc3cac1743398a7e91157c0d40d1903b051a11ae
Askaell <andrey.b176@gmail.com>
2021-05-02 10:16:34 +0300

    fixes from PR

--- pkg/models/category.go
+++ pkg/models/category.go
@@ -14,18 +14,25 @@ func NewCategory(id int, slug string, name string, parent *Category) *Category {
 	c.Id = id
 	c.Slug = slug
 	c.Name = name
-	c.Childs = make([]*Category, 0)
+	c.Childs = make(Categories, 0)
 
 	if parent == nil {
-		c.ParentId = id
-		c.Parent = c
+		c.ParentId = 0
+		c.Parent = nil
 		return c
 	}
 
 	c.ParentId = parent.Id
 	c.Parent = parent
+	c.appendChildToParent()
 
 	return c
+}
+
+func (c *Category) appendChildToParent() {
+	if c.Parent != nil {
+		c.Parent.Childs = append(c.Parent.Childs, c)
+	}
 }
 
 func (c *Category) GetRootParent() *Category {
@@ -35,9 +42,29 @@ func (c *Category) GetRootParent() *Category {
 	return c.Parent.GetRootParent()
 }
 
-func (c *Category) GetBreadCrumbs() []*Category {
+func (c *Category) GetBreadCrumbs() Categories {
 	if c.Id == c.ParentId {
 		return []*Category{c}
 	}
 	return append(c.Parent.GetBreadCrumbs(), c)
+}
+
+type Categories []*Category
+
+func (cs *Categories) Names() (names []string) {
+	for _, c := range *cs {
+		if c != nil {
+			names = append(names, c.Name)
+		}
+	}
+	return names
+}
+
+func (cs *Categories) Slugs() (slugs []string) {
+	for _, c := range *cs {
+		if c != nil {
+			slugs = append(slugs, c.Slug)
+		}
+	}
+	return slugs
 }
